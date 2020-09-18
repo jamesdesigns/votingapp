bf5723eeb9de3c1739f8fd3f02835c6ea0322056
James Hooper <jameshooperdesigns@gmail.com>
2020-09-18 16:55:10 -0600

    Add the required dependecies to the app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+// Set the public folder
+app.use()
