df5a88a44a3b073e444b344675bfda5858482963
James Hooper <jameshooperdesigns@gmail.com>
2020-09-18 23:17:42 -0600

    Add router to the poll.js file

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+
 const app = express()
+const poll = require('./routes/poll')
 
 // Set the public folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -13,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 // Enable CORS
 app.use(cors())
+app.use('/poll', poll)
 
 const port = 3000;
 
