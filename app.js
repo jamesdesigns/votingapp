10bad2324be39a7b64fa4ecbf6c7cafc4f4de2d0
James Hooper <jameshooperdesigns@gmail.com>
2020-09-18 21:39:43 -0600

    Add public folder and setup the enabling of CORS, Body Parser, and the server

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ const cors = require('cors')
 const app = express()
 
 // Set the public folder
-app.use()
+app.use(express.static(path.join(__dirname, 'public')))
+// Body Parser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Enable CORS
+app.use(cors())
+
+const port = 3000;
+
+// Start the Server
+app.listen(port, () => console.log(`Server started on port ${port}`))
