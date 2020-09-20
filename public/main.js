f87a8257eacbd9cc07915c6998034c0d22a4f069
James Hooper <jameshooperdesigns@gmail.com>
2020-09-19 22:53:51 -0600

    Add Pusher to the main.js script

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('vote-form')
-
+// Form Submit Event
 form.addEventListener('submit', (e) => {
     const choice = document.querySelector('input[name=fed]:checked').value
     const data = {fed: choice}
@@ -18,3 +18,48 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
 
 })
+
+let dataPoints = [
+    { label: 'James', y: 0 },
+    { label: 'Doug', y: 0 },
+    { label: 'Cristin', y: 0 },
+    { label: 'Caleb', y: 0 }
+]
+
+const chartContainer = document.querySelector('#chartContainer')
+if(chartContainer) {
+    const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        theme: 'theme1',
+        title: {
+            text: 'FED Results'
+        },
+        data: [
+            {
+                type: 'column',
+                dataPoints: dataPoints
+            }
+        ]
+    })
+    chart.render()
+
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('4bc8ef10221bac6c9838', {
+        cluster: 'us2'
+    });
+
+    var channel = pusher.subscribe('fed-poll');
+    channel.bind('fed-vote', function(data) {
+       dataPoints = dataPoints.map(x => {
+           if(x.label == data.fed) {
+                x.y += data.points
+                return x
+           } else {
+               return x
+           }
+       })
+       chart.render()
+    });
+}
