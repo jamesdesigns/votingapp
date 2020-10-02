e210a90290acda4ca59e6dcd4c1c73ab7828f864
James Hooper <jameshooperdesigns@gmail.com>
2020-10-02 17:33:49 -0600

    Edit the config information

--- config/db.js
+++ config/db.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 // Map global promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
-mongoose.connect('mongo "mongodb+srv://cluster0.8oibv.mongodb.net/Cluster0" --username james')
+mongoose.connect('')
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err))
